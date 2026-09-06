edf95da094f704236a6128a030d3d715a555004e
ZmajSoft <zmmaj@hotmail.com>
2026-09-06 22:16:19 +0200

    Add functions to check binary image data

--- img_parser.h
+++ img_parser.h
@@ -8,4 +8,7 @@
 cJSON* parse_srcset(const char *srcset_str);
 void calculate_image_dimensions(cJSON *image_json);
 
+int is_binary_image_data(const unsigned char *buffer, size_t size);
+int is_binary_data(const unsigned char *buffer, size_t size);
+
 #endif // IMG_PARSER_H
